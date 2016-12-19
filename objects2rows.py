10361a2b16b15d77bb95d35973fb5e900fca6beb
Jens Stimpfle <jfs@jstimpfle.de>
2016-12-19 01:27:25 +0100

    factor out tests

--- objects2rows.py
+++ objects2rows.py
@@ -6,75 +6,42 @@ def add_rows(database, query, columns, rows):
     database[query.table] += [tuple(row[i] for i in key) for row in rows]
 
 
-def todb_value(columns, rows, objects, spec, database):
+def todb_value(columns, rows, objs, spec, database):
     idx = columns.index(spec.variable)
-    for row, obj in zip(rows, objects):
+    for row, obj in zip(rows, objs):
         row[idx].set(obj)
 
 
-def todb_struct(columns, rows, objects, spec, database):
+def todb_struct(columns, rows, objs, spec, database):
     if spec.query is not None:
         nextcolumns = columns + spec.query.freshvariables
-        nextrows = [row + [Settable() for _ in spec.query.freshvariables] for row in rows]
+        nextrows = [row + [Settable() for _ in spec.query.freshvariables] for row, obj in zip(rows, objs) if obj is not None]
         add_rows(database, spec.query, nextcolumns, nextrows)
     else:
         nextcolumns = columns
         nextrows = rows
     for key in spec.childs:
-        nextobjects = []
-        for obj in objects:
-            nextobjects.append(obj[key])
-        todb(nextcolumns, nextrows, nextobjects, spec.childs[key], database)
+        nextobjs = [obj[key] for obj in objs if obj is not None]
+        todb(nextcolumns, nextrows, nextobjs, spec.childs[key], database)
 
 
-def todb_list(columns, rows, objects, spec, database):
+def todb_list(columns, rows, objs, spec, database):
     nextcolumns = columns + spec.query.freshvariables
     nextrows = []
-    nextobjects = []
-    for row, obj in zip(rows, objects):
-        for item in obj:
+    nextobjs = []
+    for row, lst in zip(rows, objs):
+        for item in lst:
             nextrows.append(row + [Settable() for _ in spec.query.freshvariables])
-            nextobjects.append(item)
-    todb(nextcolumns, nextrows, nextobjects, spec.childs['_val_'], database)
+            nextobjs.append(item)
+    todb(nextcolumns, nextrows, nextobjs, spec.childs['_val_'], database)
     add_rows(database, spec.query, nextcolumns, nextrows)
 
 
-def todb(columns, rows, objects, spec, database):
+def todb(columns, rows, objs, spec, database):
+    assert len(rows) == len(objs)
     if spec.typ == Value:
-        todb_value(columns, rows, objects, spec, database)
+        todb_value(columns, rows, objs, spec, database)
     elif spec.typ == Struct:
-        todb_struct(columns, rows, objects, spec, database)
+        todb_struct(columns, rows, objs, spec, database)
     elif spec.typ == List:
-        todb_list(columns, rows, objects, spec, database)
-
-
-a = Spec(Value, None, None, 'a')
-b = Spec(Value, None, None, 'b')
-c = Spec(Value, None, None, 'c')
-d = Spec(Value, None, None, 'd')
-s = Spec(Struct, { 'a': a, 'b': b }, Query(['a', 'b'], 'foo', ['a', 'b', 'c']))
-s2 = Spec(Struct, { 'c': c, 'd': d, 's': s }, None)
-l = Spec(List, { '_val_': s2 }, Query(['c', 'd'], 'bar', ['c', 'd']))
-
-database = {
-    'foo': [],
-    'bar': []
-}
-
-myobjects = [[
-    { 'c': 1, 'd': 3, 's': { 'a': 666, 'b': 1024 } },
-    { 'c': 2, 'd': 3, 's': { 'a': 666, 'b': 1024 } },
-    { 'c': 3, 'd': 3, 's': { 'a': 666, 'b': 1024 } }
-]]
-
-todb([], [[] for _ in myobjects], myobjects, l, database)
-
-print('foo')
-print('===')
-print(database['foo'])
-print()
-
-print('bar')
-print('===')
-print(database['bar'])
-print()
+        todb_list(columns, rows, objs, spec, database)
---
+++ tests.py
@@ -0,0 +1,98 @@
+import json
+
+from types import Value, Struct, List, Spec, Query
+import rows2objects
+import objects2rows
+
+
+a = Spec(Value, None, None, 'a')
+b = Spec(Value, None, None, 'b')
+c = Spec(Value, None, None, 'c')
+d = Spec(Value, None, None, 'd')
+s = Spec(Struct, { 'a': a, 'b': b }, Query(['a', 'b'], 'foo', ['a', 'b', 'c']))
+s2 = Spec(Struct, { 'c': c, 'd': d, 's': s }, None)
+lst = Spec(List, { '_val_': s2 }, Query(['c', 'd'], 'bar', ['c', 'd']))
+
+mydatabase = {
+    'foo': [(1, 2, 3), (4, 5, 6)], 
+    'bar': [(3, 666), (6, 1024), (42, 0)]
+}
+
+myobject = [
+    { 'c': 3, 'd': 666, 's': { 'a': 1, 'b': 2 } },
+    { 'c': 6, 'd': 1024, 's': { 'a': 4, 'b': 5 } },
+    { 'c': 42, 'd': 0, 's': None }
+]
+
+
+def json_repr(x):
+    return json.dumps(x, sort_keys=True, indent=2)
+
+
+def test_rows2objects():
+    print()
+    print('TESTING rows2objects()...')
+    print('=========================')
+    print('')
+
+    print('Database:')
+    print('=========')
+    for key, rows in sorted(mydatabase.items()):
+        print(key)
+        print('-' * len(key))
+        for row in sorted(rows):
+            print(row)
+        print()
+
+    [(topobject, subobject)] = rows2objects.fromdb([], [()], [None], lst, mydatabase)
+
+    assert topobject is None
+    assert isinstance(subobject, list)
+
+    print('RESULT')
+    print('======')
+    print(json_repr(subobject))
+
+    return subobject
+
+
+def test_objects2rows():
+    print()
+    print('TESTING objects2rows()...')
+    print('=========================')
+    print('')
+
+    database = {
+        'foo': [],
+        'bar': []
+    }
+
+    print('Objects:')
+    print('========')
+    print(json_repr(myobject))
+
+    objects2rows.todb([], [[]], [myobject], lst, database)
+
+    print()
+    print('RESULTS')
+    print()
+
+    for table in ['bar', 'foo']:
+        print(table)
+        print('=' * len(table))
+        for row in database[table]:
+            print(row)
+        print()
+
+    return database
+
+
+if __name__ == '__main__':
+    objects = test_rows2objects()
+    database = test_objects2rows()
+    database = dict((key, [tuple(val.get() for val in row) for row in rows]) for key, rows in database.items())
+    for table in database.values():
+        table.sort()
+
+    assert json_repr(objects) == json_repr(myobject)
+    assert json_repr(database) == json_repr(mydatabase)
