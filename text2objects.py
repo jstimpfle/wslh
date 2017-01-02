9596d0fc38c611ec06cfd5171a0048d9a19beae4
Jens Stimpfle <jfs@jstimpfle.de>
2017-01-02 04:18:32 +0100

    move "indent" variable to a more static scope

--- text2objects.py
+++ text2objects.py
@@ -3,6 +3,9 @@ import re
 from datatypes import Value, Struct, List, Dict
 
 
+INDENTSPACES = 4
+
+
 class ParseException(Exception):
     def __init__(self, msg, text, lineno, charno):
         self.msg = msg
@@ -37,7 +40,7 @@ def parse_newline(text, i):
     return i + 1
 
 
-def parse_keyword(indent, text, i):
+def parse_keyword(text, i):
     end = len(text)
     start = i
     while i < end and text[i].isalpha():
@@ -47,7 +50,7 @@ def parse_keyword(indent, text, i):
     return i, text[start:i]
 
 
-def parse_identifier(indent, text, i):
+def parse_identifier(text, i):
     end = len(text)
     start = i
     m = re.match(r'^[a-zA-Z][a-zA-Z0-9]*', text[i:])
@@ -57,7 +60,7 @@ def parse_identifier(indent, text, i):
     return i, text[start:i]
 
 
-def parse_string(indent, text, i):
+def parse_string(text, i):
     end = len(text)
     start = i
     m = re.match(r'^\[[a-zA-Z 0-9 _-]*\]', text[i:])
@@ -67,7 +70,7 @@ def parse_string(indent, text, i):
     return i, text[start+1:i-1]
 
 
-def parse_int(indent, text, i):
+def parse_int(text, i):
     end = len(text)
     start = i
     m = re.match(r'^(0|-?[1-9][0-9]*)', text[i:])
@@ -78,7 +81,6 @@ def parse_int(indent, text, i):
 
 
 def parse_block(dct, indent, text, i):
-    nextindent = indent + 4
     end = len(text)
     out = []
     while True:
@@ -89,43 +91,43 @@ def parse_block(dct, indent, text, i):
         if not text[i:].startswith(' ' * indent):
             break
         i += indent
-        i, kw = parse_keyword(indent, text, i)
+        i, kw = parse_keyword(text, i)
         parser = dct.get(kw)
         if parser is None:
             raise make_parse_exc('Found unexpected field "%s"' %(kw,), text, i)
-        i, val = parser(nextindent, text, i)
+        i, val = parser(text, i)
         out.append((kw, val))
     return i, out
 
 
 def space_and_then(valueparser):
-    def space_and_then_parser(indent, text, i):
+    def space_and_then_parser(text, i):
         i = parse_space(text, i)
-        i, v = valueparser(indent, text, i)
+        i, v = valueparser(text, i)
         return i, v
     return space_and_then_parser
 
 
 def newline_and_then(valueparser):
-    def newline_and_then_parser(indent, text, i):
+    def newline_and_then_parser(text, i):
         i = parse_newline(text, i)
-        i, v = valueparser(indent, text, i)
+        i, v = valueparser(text, i)
         return i, v
     return newline_and_then_parser
 
 
 def make_keyvalue_parser(keyparser, valueparser):
-    def keyvalue_parser(indent, text, i):
+    def keyvalue_parser(text, i):
         i = parse_space(text, i)
-        i, k = keyparser(indent, text, i)
+        i, k = keyparser(text, i)
         i = parse_newline(text, i)
-        i, v = valueparser(indent, text, i)
+        i, v = valueparser(text, i)
         return i, (k, v)
     return keyvalue_parser
 
 
-def make_struct_parser(dct):
-    def struct_parser(indent, text, i):
+def make_struct_parser(dct, indent):
+    def struct_parser(text, i):
         i, items = parse_block(dct, indent, text, i)
         wantkeys = set(dct.keys())
         havekeys = set(k for k, v in items)
@@ -143,9 +145,9 @@ def make_struct_parser(dct):
     return struct_parser
 
 
-def make_dict_parser(key_parser, val_parser):
+def make_dict_parser(key_parser, val_parser, indent):
     item_parser = make_keyvalue_parser(key_parser, val_parser)
-    def dict_parser(indent, text, i):
+    def dict_parser(text, i):
         i, items = parse_block({ 'value': item_parser }, indent, text, i)
         out = {}
         for _, (k, v) in items:
@@ -156,8 +158,8 @@ def make_dict_parser(key_parser, val_parser):
     return dict_parser
 
 
-def make_list_parser(parser):
-    def list_parser(indent, text, i):
+def make_list_parser(parser, indent):
+    def list_parser(text, i):
         dct = { 'value': parser }
         i, items = parse_block(dct, indent, text, i)
         out = []
@@ -168,13 +170,17 @@ def make_list_parser(parser):
 
 
 def doparse(parser, text):
-    i, r = parser(0, text, 0)
+    i, r = parser(text, 0)
     if i != len(text):
         raise make_parse_exc('Unconsumed text', text, i)
     return r
 
 
-def make_parser_from_spec(lookup_primparser, spec):
+def make_parser_from_spec(lookup_primparser, spec, indent=None):
+    if indent is None:
+        indent = 0
+
+    nextindent = indent + INDENTSPACES
     typ = type(spec)
 
     if typ == Value:
@@ -182,27 +188,30 @@ def make_parser_from_spec(lookup_primparser, spec):
         if parser is None:
             raise ValueError('There is no parser for datatype "%s"' %(spec.primtype,))
         return parser
+
     elif typ == Struct:
         dct = {}
         for k, v in spec.childs.items():
-            subparser = make_parser_from_spec(lookup_primparser, v)
+            subparser = make_parser_from_spec(lookup_primparser, v, nextindent)
             if type(v) == Value:
                 dct[k] = space_and_then(subparser)
             else:
                 dct[k] = newline_and_then(subparser)
-        return make_struct_parser(dct)
+        return make_struct_parser(dct, indent)
+
     elif typ == List:
-        val_parser = make_parser_from_spec(lookup_primparser, spec.childs['_val_'])
+        val_parser = make_parser_from_spec(lookup_primparser, spec.childs['_val_'], nextindent)
         for k, v in spec.childs.items():
-            subparser = make_parser_from_spec(lookup_primparser, v)
+            subparser = make_parser_from_spec(lookup_primparser, v, nextindent)
             if type(v) == Value:
                 p = space_and_then(subparser)
             else:
                 p = newline_and_then(subparser)
-        return make_list_parser(p)
+        return make_list_parser(p, indent)
+
     elif typ == Dict:
-        key_parser = make_parser_from_spec(lookup_primparser, spec.childs['_key_'])
-        val_parser = make_parser_from_spec(lookup_primparser, spec.childs['_val_'])
-        return make_dict_parser(key_parser, val_parser)
+        key_parser = make_parser_from_spec(lookup_primparser, spec.childs['_key_'], nextindent)
+        val_parser = make_parser_from_spec(lookup_primparser, spec.childs['_val_'], nextindent)
+        return make_dict_parser(key_parser, val_parser, indent)
 
     assert False  # missing case
